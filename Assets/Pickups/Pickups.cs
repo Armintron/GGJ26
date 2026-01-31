99ade23d063b0f8a110aebc11e8c31411411c290
Jeron Artest <73331318+jeronartest@users.noreply.github.com>
2026-01-30 18:57:47 -0800

    feat: pickups

---
+++ Assets/Pickups/Pickups.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public enum PickupType { Oxygen }
+    
+    [Header("Pickup Settings")]
+    public PickupType type;
+    public float value = 20f;
+    public float interactDistance = 5f;
+
+    public bool CanInteract { get; private set; }
+    private Transform playerCamera;
+
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            playerCamera = Camera.main.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (playerCamera == null)
+        {
+            CanInteract = false;
+            return;
+        }
+
+        // Use a raycast from the camera center to see if we are looking at this object specifically
+        Ray ray = new Ray(playerCamera.position, playerCamera.forward);
+        RaycastHit hit;
+        
+        bool lookingAtThis = false;
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                lookingAtThis = true;
+            }
+        }
+
+        CanInteract = lookingAtThis;
+
+        if (CanInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            ActivatePickup();
+        }
+    }
+
+    private void ActivatePickup()
+    {
+        Debug.Log($"Activated {type} pickup!");
+        
+        switch (type)
+        {
+            case PickupType.Oxygen:
+                ApplyOxygen();
+                break;
+        }
+
+        // Object stays in the scene as requested
+    }
+
+    private void ApplyOxygen()
+    {
+        // Placeholder for oxygen logic
+        // Example: PlayerStats.Instance.AddOxygen(value);
+        Debug.Log($"+{value} Oxygen");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Visualize interaction range in editor
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactDistance);
+    }
+}
